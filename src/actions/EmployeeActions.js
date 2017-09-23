efcc43bba99f991310a83911a9a4247ac011cb4c
sxcw <yuan329419@gmail.com>
2017-09-23 15:56:38 -0500

    Manager - Add employcreate action creator

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,8 +1,15 @@
-import { EMPLOYEE_UPDATE } from './type';
+import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE } from './type';
 
 export const employeeUpdate = ({ prop, value }) => {
   return {
     type: EMPLOYEE_UPDATE,
     payload: { prop, value }
+  };
+}
+
+export const employeeCreate = ({ name, phone, shift }) => {
+  return {
+    type: EMPLOYEE_CREATE,
+    payload: { name, phone, shift }
   };
 }
--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,10 +1,14 @@
 import React , { Component } from 'react';
 import { Picker, Text } from 'react-native';
 import { connect } from 'react-redux';
-import { employeeUpdate } from '../actinos';
+import { employeeUpdate, employeeCreate } from '../actinos';
 import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
+  onButtonPress() {
+    const { name, phone, shift } = this.props;
+    this.props.employeeCreate({ name, phone, shift });
+  }
   render() {
     return (
       <Card>
@@ -44,7 +48,7 @@ class EmployeeCreate extends Component {
         </CardSection>
 
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Create
           </Button>
         </CardSection>
@@ -67,4 +71,4 @@ const mapStateToProps = (state) => {
   return { name, phone, shift };
 };
 
-export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
+export default connect(mapStateToProps, { employeeUpdate, employeeCreate })(EmployeeCreate);
