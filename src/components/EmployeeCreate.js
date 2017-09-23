60286388af203a12b84005216f99de6e6f73b504
sxcw <yuan329419@gmail.com>
2017-09-22 22:46:32 -0500

    Manager - Add custom styles to CardSection component

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -26,7 +26,7 @@ class EmployeeCreate extends Component {
           />
         </CardSection>
 
-        <CardSection>
+        <CardSection style={{ flexDirection: 'column' }}>
           <Text style={styles.pickerTextStyle}>Shift</Text>
           <Picker
             style={{ flex : 1 }}
