16c4767bbfd93dc2ff5f67a90420d1d20ab43dde
sxcw <yuan329419@gmail.com>
2017-09-25 21:43:24 -0500

    Manager - Fix employeeCreate action creator

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -9,5 +9,7 @@ export const employeeUpdate = ({ prop, value }) => {
 }
 
 export const employeeCreate = ({ name, phone, shift }) => {
-  firebase.database.ref('users/userId/employees')
+  const { currentUser } = firebase.auth();
+  firebase.database().ref(`users/${currentUser.uid}/employees`)
+    .push({ name, phone, shift });
 }
