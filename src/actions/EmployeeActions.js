b1aa1b83a06a669f77d0b94e1b70e0e0da33282a
sxcw <yuan329419@gmail.com>
2017-09-28 18:21:18 -0500

    Manager - Add fetch employees action creator

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
-import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE } from './type';
+import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE, EMPLOYEES_FETCH_SUCCESS } from './type';
 
 export const employeeUpdate = ({ prop, value }) => {
   return {
@@ -19,5 +19,16 @@ export const employeeCreate = ({ name, phone, shift }) => {
         dispatch({ type: EMPLOYEE_CREATE });
         Actions.employeeList(type: 'reset')
       });
+  }
+}
+
+export const employeesFetch = () => {
+  const { currentUser } = firebase.auth();
+
+  return (dispatch) => {
+    firebase.database().ref(`users/${currentUser.uid}/employees`)
+      .on('value', snapshot => {
+        dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() });
+      })
   }
 }
